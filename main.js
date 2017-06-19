646b339353e341570013dd4c7fa8d60b81833b9e
bytecrash <amk9775@gmail.com>
2017-06-20 01:14:59 +0430

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-var Bot = require('389470588:AAGS-8jQGNlSTsPKyfSMcxf0CtQLswDCaTY');
- 
-var TOKEN = 'TOKEN';
-var bot = new Bot(TOKEN, { polling: true });
- 
-bot.onText(/\/?start/, function(msg) {
-    bot.sendMessage(msg.chat.id, 'به من بگی ping می‌گم pong و بالعکس! :)');
+var TelegramBot = require('node-telegram-bot-api');
+
+var token = '389470588:AAGS-8jQGNlSTsPKyfSMcxf0CtQLswDCaTY';
+// Setup polling way
+var bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/insult/, function(msg, match) {
+  var fromId = msg.from.id;
+  var insults = ["Dumbass", "Out of 100,000 sperm, you were the fastest?", "Look, you aint funny. Your life is just a joke."];
+  var chosenInsult = insults[Math.floor(Math.random() * insults.length)];
+  bot.sendMessage(fromId, chosenInsult);
 });
- 
-bot.onText(/\/?ping/, function(msg) {
-    bot.sendMessage(msg.chat.id, 'pong');
-});
- 
-bot.onText(/\/?pong/, function(msg) {
-    bot.sendMessage(msg.chat.id, 'ping');
+
+bot.onText(/\/help/, function(msg, match) {
+  var fromId = msg.from.id;
+  bot.sendMessage(fromId, "This spectacular bot just have one single command.\n/insult - Insult you.");
 });
